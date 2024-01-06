1a5c3c6c3a373696d232d3e3aa1bac0f65f17d99
habed <hamza.abed.fr@gmail.com>
2024-01-06 21:42:02 +0100

    chore: main

--- main.go
+++ main.go
@@ -8,16 +8,31 @@ import (
 
 	"github.com/Scalingo/go-handlers"
 	"github.com/Scalingo/go-utils/logger"
+	"github.com/Scalingo/sclng-backend-test-v1/migration"
+	"github.com/Scalingo/sclng-backend-test-v1/util"
+	"github.com/Scalingo/sclng-backend-test-v1/worker"
 )
 
 func main() {
 	log := logger.Default()
 	log.Info("Initializing app")
-	cfg, err := newConfig()
+	cfg, err := util.NewConfig()
 	if err != nil {
 		log.WithError(err).Error("Fail to initialize configuration")
 		os.Exit(1)
 	}
+
+	// connect DB
+	db := util.ConnectPGDB(cfg, log)
+
+	// migrate database if environment dev
+	if cfg.Env == "dev" {
+		migration.Migrate(db, log, cfg.EraseDbWhenMigrate)
+	}
+
+	// start worker fetching
+	mainWorker := worker.NewMainWorker(db, log, cfg.GithubToken)
+	go mainWorker.JobRoutine()
 
 	log.Info("Initializing routes")
 	router := handlers.NewRouter(log)
